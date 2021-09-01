974a0af7d5cdabbe55fc6db52ca476f9002a0f11
VanessaMorenoTorres <morenot.vanessa@gmail.com>
2021-09-01 10:37:39 -0500

    change in style and added image

--- reactstaticcomponents/src/bigBox.js
+++ reactstaticcomponents/src/bigBox.js
@@ -7,7 +7,7 @@ function Bbox(prop){
         <div className="big">
             <h3>{prop.title}</h3>
             <p>821</p>
-            <a src="">Picture here</a>
+            <img src='https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/36414330/original/8ba2103e3d16c5dd9a7035781e3505d85478d001/design-apa-styled-single-case-design-graphs.png' className="pic"/>
         </div>
     )
 }
